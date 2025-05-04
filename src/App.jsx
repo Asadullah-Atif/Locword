92f8dac08e72242bf5cbf39e0658afd8c5736c45
Asadullah-Atif <meharatif262@gmail.com>
2025-05-04 05:54:07 +0500

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,39 @@ function App() {
   let lowercase = useRef(0);
   let number = useRef(0);
   let symbols = useRef(0);
-  let range = useRef(0);
+
+  const [range, setRange] = useState(12);
+  function handleRangeValue(totalPasswordLength) {
+    setRange(totalPasswordLength);
+    console.log("Will set range to:", totalPasswordLength);
+  }
+
+
+  function handleGenerateButton() {
+    let uppercaseValue = uppercase.current.checked;
+    let lowercaseValue = lowercase.current.checked;
+    let numberValue = number.current.checked;
+    let symbolsValue = symbols.current.checked;
+
+    if (uppercaseValue || lowercaseValue || numberValue || symbolsValue) {
+      console.log("Checked!");
+      let password = "";
+      let str = "";
+      if (uppercaseValue) str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      console.log(str);
+
+      for(let i = 0; i<str.length;i++){
+          password += Math.floor(Math.random()*16);
+          console.log(password);
+
+      }
+      if (lowercaseValue) str += "abcdefghijklmnopqrstuvwxyz";
+      if (numberValue) str += "0123456789";
+      if (symbolsValue) str += "!@#$%^&*()-_=+[]{}|;:',.<>?/`~/";
+    } else {
+      alert("Check at least one.");
+    }
+  }
 
   return (
     <>
@@ -19,36 +51,40 @@ function App() {
         </div>
         <div className="characterContainer">
           <CharacterAddSuggestions
-            suggestionType="Uppercase"
+            suggestionType="uppercase"
             fromTo="(A-Z)"
             checkedValue={true}
             inputRef={uppercase}
             min={4}
             max={8}
+            changeValue={handleRangeValue}
           />
           <CharacterAddSuggestions
-            suggestionType="Lowercase"
+            suggestionType="lowercase"
             fromTo="(a-z)"
             checkedValue={true}
             inputRef={lowercase}
             min={4}
             max={8}
+            changeValue={handleRangeValue}
           />
           <CharacterAddSuggestions
-            suggestionType="Number"
+            suggestionType="number"
             fromTo="(0-9)"
             checkedValue={true}
             inputRef={number}
             min={2}
             max={6}
+            changeValue={handleRangeValue}
           />
           <CharacterAddSuggestions
-            suggestionType="Symbols"
+            suggestionType="symbols"
             fromTo="(!@#$%^&*)"
             checkedValue={false}
             inputRef={symbols}
             min={2}
             max={6}
+            changeValue={handleRangeValue}
           />
         </div>
         <button
--- src/Components/ChractersAddSugestion.jsx
+++ src/Components/ChractersAddSugestion.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+let objOfLength = {
+  uppercase: 4,
+  lowercase: 4,
+  number: 2,
+  symbols: 2,
+};
 export function CharacterAddSuggestions({
   suggestionType,
   fromTo,
@@ -7,9 +13,11 @@ export function CharacterAddSuggestions({
   inputRef,
   min,
   max,
+  changeValue,
 }) {
   let [checkboxValue, setCheckboxValue] = useState(checkedValue);
   let [value, setValue] = useState(min);
+
   function handleCheckbox() {
     if (checkboxValue) {
       setCheckboxValue(false);
@@ -20,12 +28,54 @@ export function CharacterAddSuggestions({
   function handleValue(e) {
     if (e.key === "ArrowUp" && value < max) {
       setValue(value + 1);
+
+      let current = e.target.id;
+      console.log(current);
+
+      switch (current) {
+        case "uppercase":
+          objOfLength.uppercase = objOfLength.uppercase + 1;
+          console.log(objOfLength.uppercase);
+          break;
+        case "lowercase":
+          objOfLength.lowercase++;
+          console.log(objOfLength.lowercase);
+          break;
+        case "number":
+          objOfLength.number++;
+          console.log(objOfLength.number);
+          break;
+        case "symbols":
+          objOfLength.symbols++;
+          console.log(objOfLength.symbols);
+          break;
+      }
     }
     if (e.key === "ArrowDown" && value > min) {
       setValue(value - 1);
+      let current = e.target.id;
+      switch (current) {
+        case "uppercase":
+          objOfLength.uppercase--;
+          console.log(objOfLength.uppercase);
+          break;
+        case "lowercase":
+          objOfLength.lowercase--;
+          console.log(objOfLength.lowercase);
+          break;
+          hi
+        case "number":
+          objOfLength.number--;
+          console.log(objOfLength.number);
+          break;
+        case "symbols":
+          objOfLength.symbols--;
+          console.log(objOfLength.symbols);
+          break;
+      }
     }
+    changeValue(objOfLength);
   }
-
   return (
     <div className={`grid-three ${suggestionType}`}>
       <input
@@ -43,11 +93,12 @@ export function CharacterAddSuggestions({
           Length:{" "}
         </label>
         <input
+          id={suggestionType}
           type="number"
           min={min}
           max={max}
           value={value}
-          onKeyDown={handleValue}
+          onKeyDown={(e) => handleValue(e)}
           autoFocus
         />
       </div>
